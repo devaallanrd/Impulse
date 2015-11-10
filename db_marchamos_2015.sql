4036a01c10efa68072e1cc0b7dd7f5ade516c2d2
Allan Rojas D <developer.aallanrd@gmail.com>
2015-11-10 13:31:12 -0600

    Update db_marchamos_2015.sql

--- db_marchamos_2015.sql
+++ db_marchamos_2015.sql
@@ -1,3 +1,23 @@
+--Cree una Base de Datos que se llame Arduino 
+
+--Cree esta tabla, note que el codigo es de 26 Digitos , es lo que leemos de la tarjeta :D
+Create table usuarios(
+  
+   code  varchar (50) primary key ,
+   id    varchar (12), 
+   propietario varchar (15),
+   automovil varchar(30) ,
+   vencido varchar (15),
+   multas int
+
+);
+--Inserte estos datos de prueba
+insert into usuarios values ('25351484850546652535357523', '1234', 'Jhonny Rojas', 'BMW 502',   'false' , 0);
+insert into usuarios values ('25351484850546657684849673', '4321', 'Pablo Zamora', 'Ford ',  'true'  , 0);
+insert into usuarios values ('25351484850546967657051543', '1324', 'Josue Pereira', 'Citroen', 'true',  0);
+
+
+
 --drop table usuarios;
 --select * from usuarios;
 --Update la columna vencido de una fila con el mismo codigo
@@ -11,20 +31,8 @@ update  usuarios
   set vencido = 'false'
   where id='1234';
 
-Create table usuarios(
-  
-   code  varchar (50) primary key ,
-   id    varchar (12), 
-   propietario varchar (15),
-   automovil varchar(30) ,
-   vencido varchar (15),
-   multas int
 
-);
 
-insert into usuarios values ('25351484850546652535357523', '1234', 'Jhonny Rojas', 'BMW 502',   'false' , 0);
-insert into usuarios values ('25351484850546657684849673', '4321', 'Pablo Zamora', 'Ford ',  'true'  , 0);
-insert into usuarios values ('25351484850546967657051543', '1324', 'Josue Pereira', 'Citroen', 'true',  0);
 
 --drop table pasos;
 --select * from pasos;
