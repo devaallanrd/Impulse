6e6c0d926a1e226f1b1a02d5fd68f8dc317849ad
Allan Rojas <allanjose91@gmail.com>
2015-11-10 12:50:56 -0600

    Version 1.0

--- WEBUI/App_Start/BundleConfig.cs
+++ WEBUI/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace WEBUI
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                         "~/Scripts/app.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/marchamos").Include(
+                        "~/Scripts/marchamos.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
