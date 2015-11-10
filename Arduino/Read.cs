b3c76c03ff5997796266aa80599fe194dc850074
Allan Rojas <allanjose91@gmail.com>
2015-11-10 13:01:45 -0600

    Arduino Read Update

--- Arduino/Read.cs
+++ Arduino/Read.cs
@@ -27,8 +27,9 @@ namespace Arduino
         //Chequear si el puerto esta disponible
         public  bool check()
         {
+
             String[] portNames = SerialPort.GetPortNames();
-            string found = Array.Find( portNames, s => s.Equals("COM4") );
+            string found = Array.Find( portNames, s => s.Equals("COM4") ); //Pasarlo por parámetro
             if (found == null)
             {
                 return false;
@@ -39,28 +40,43 @@ namespace Arduino
             }
         }
 
+       //Lectura en el puerto para esperar mensaje del arduino
         public  String read()
         {
-            using (SerialPort sp = new SerialPort("COM4", 9600))
+            using (SerialPort sp = new SerialPort("COM4", 9600)) // Pasarlos por parámetro
             {
                 try
                 {
+                    //Abre el puerto especifico 
                     sp.Open();
+
+                    //Preparamos variables 
                     string returnMessage = "";
                     int intReturnASCII = 0;
 
+                    /* Enciclamos el sp.ReadByte(); para obtener los bytes desde el arduino
+                     * cada byte se pega a la linea de caracteres de retorno.
+                     *  ## Esto deberia ser una tarea asyncrona ·##
+                    */
                     while (true)
                     {
+                        //Lee un byte
                         intReturnASCII = sp.ReadByte();
+
+                        //Cuando lee un byte asigna a la cadena de retorno toda la cadena + el byte 
                         returnMessage = returnMessage + Convert.ToChar(intReturnASCII);
-                        if (returnMessage.Length == 26)
+
+                        //La cantidad de caracteres que queremos leer del codigo de la tarjeta
+                        if (returnMessage.Length == 26) // Pasarlo por parámetro
                         {
                             sp.Close();
                             return returnMessage;
 
                         }
                     }
-                } catch(Exception e){
+                } 
+                //Si encuentra un error en el proceso
+                catch(Exception e){
                     sp.Close();
                     return "";
                 }
